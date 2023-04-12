833c7d54ef5c55e306495a3fb6885ca6237e599b
SATE000 <lcw200620@gmail.com>
2023-04-13 00:41:14 +0900

    첫번째 커밋 (23.04.13)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Study_Spring
+
+## 커밋 내용 정리
+
+1번 커밋 : WAS에 대해 이해하고 기본적인 HttpServlet과 HttpServletRequest의 기본적인 사용 방법 익히기 <br/>
--- src/main/java/hello/servelt/ServeltApplication.java
+++ src/main/java/hello/servelt/ServeltApplication.java
@@ -2,7 +2,9 @@ package hello.servelt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //서블릿들을 자동으로 등록을 해주는 Annotation
 @SpringBootApplication
 public class ServeltApplication {
 
